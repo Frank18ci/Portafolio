0d5ed67614bf6d354408f78cb070223026fbdaba
Frank <fcarpio113@gmail.com>
2025-07-02 19:44:01 -0500

    fisrt commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+
+export const routes: Routes = [
+    {
+        path: '', 
+        component: MainComponent
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+];

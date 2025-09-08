fce93902616435b39204b0175c3a70eb0cee9c07
Frank <fcarpio113@gmail.com>
2025-09-08 07:30:18 -0500

    feat: add habilidad-card component with HTML, SCSS, and TypeScript files

---
+++ src/app/components/habilidad-card/habilidad-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habilidad-card',
+  imports: [],
+  templateUrl: './habilidad-card.component.html',
+  styleUrl: './habilidad-card.component.scss'
+})
+export class HabilidadCardComponent {
+
+}

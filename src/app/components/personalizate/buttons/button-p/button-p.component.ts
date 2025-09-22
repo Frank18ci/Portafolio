ca6ef9326be7f80d8a5fe075471905d6be6e0d7c
Frank <fcarpio113@gmail.com>
2025-09-21 22:35:20 -0500

    feat: implement button components with HTML, SCSS, and TypeScript files

---
+++ src/app/components/personalizate/buttons/button-p/button-p.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-p',
+  imports: [],
+  templateUrl: './button-p.component.html',
+  styleUrl: './button-p.component.scss'
+})
+export class ButtonPComponent {
+
+}

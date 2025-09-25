625f19e7deeef6031f3489a49771f357c21f990e
Frank <fcarpio113@gmail.com>
2025-09-24 21:43:39 -0500

    feat: update button components to accept dynamic color input and integrate into main component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { ButtonSComponent } from '../../components/personalizate/buttons/button-s/button-s.component';
+import { ButtonPComponent } from '../../components/personalizate/buttons/button-p/button-p.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, ButtonSComponent, ButtonPComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
